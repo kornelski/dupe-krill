4cd9862d9b6b9cc39d86315dfe401be6da5f63d0
Kornel <kornel@geekhood.net>
2017-03-31 15:43:56 +0100

    Nicer path display

--- src/dupe.rs
+++ src/dupe.rs
@@ -211,7 +211,7 @@ impl Scanner {
                 debug_assert!(!temp_path.exists());
                 debug_assert!(source_path.exists());
                 debug_assert!(dest_path.exists());
-                println!("Replaced {} with {}", dest_path.display(), source_path.display());
+                println!("Hardlinked {}", combined_paths(&dest_path, &source_path));
                 merged_paths.push(dest_path);
             }
         }
@@ -224,6 +224,49 @@ impl Scanner {
             (*tmp).clone()
         }).collect()
     }
+}
+
+fn combined_paths(base: &Path, relativize: &Path) -> String {
+    let base: Vec<_> = base.iter().collect();
+    let relativize: Vec<_> = relativize.iter().collect();
+
+    let mut out = String::with_capacity(80);
+    let mut prefix_len = 0;
+    for (comp,_) in base.iter().zip(relativize.iter()).take_while(|&(a,b)| a==b) {
+        prefix_len += 1;
+        let comp = comp.to_string_lossy();
+        out += &comp;
+        if comp != "/" {
+            out.push('/');
+        }
+    }
+
+    let suffix: Vec<_> = base.iter().skip(prefix_len).rev().zip(relativize.iter().skip(prefix_len).rev())
+        .take_while(|&(a,b)| a==b).map(|(_,b)|b.to_string_lossy()).collect();
+
+    let base_unique: Vec<_> = base[prefix_len..base.len()-suffix.len()].iter().map(|b|b.to_string_lossy()).collect();
+
+    out.push('{');
+    if base_unique.is_empty() {
+        out.push('.');
+    } else {
+        out += &base_unique.join("/");
+    }
+    out += " => ";
+
+    let rel_unique: Vec<_> = relativize[prefix_len..relativize.len()-suffix.len()].iter().map(|b|b.to_string_lossy()).collect();
+    if rel_unique.is_empty() {
+        out.push('.');
+    } else {
+        out += &rel_unique.join("/");
+    }
+    out.push('}');
+
+    for comp in suffix {
+        out.push('/');
+        out += &comp;
+    }
+    out
 }
 
 #[test]
@@ -256,3 +299,20 @@ fn scan_hardlink() {
     assert_eq!(dupes[0].paths.len(), 2);
 }
 
+#[test]
+fn combined_test() {
+    let a: PathBuf = "foo/bar/baz/a.txt".into();
+    let b: PathBuf = "foo/baz/quz/zzz/a.txt".into();
+    let c: PathBuf = "foo/baz/quz/zzz/b.txt".into();
+    let d: PathBuf = "b.txt".into();
+    let e: PathBuf = "e.txt".into();
+    let f: PathBuf = "/foo/bar/baz/a.txt".into();
+    let g: PathBuf = "/foo/baz/quz/zzz/a.txt".into();
+
+    assert_eq!(&combined_paths(&a,&b), "foo/{bar/baz => baz/quz/zzz}/a.txt");
+    assert_eq!(&combined_paths(&c,&b), "foo/baz/quz/zzz/{b.txt => a.txt}");
+    assert_eq!(&combined_paths(&c,&d), "{foo/baz/quz/zzz => .}/b.txt");
+    assert_eq!(&combined_paths(&d,&c), "{. => foo/baz/quz/zzz}/b.txt");
+    assert_eq!(&combined_paths(&d,&e), "{b.txt => e.txt}");
+    assert_eq!(&combined_paths(&f,&g), "/foo/{bar/baz => baz/quz/zzz}/a.txt");
+}
